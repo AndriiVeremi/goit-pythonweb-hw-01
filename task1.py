211ea61508279b6950e2c31a9ab1b7d5faa3eacb
Andrii Veremii <114438382+AndriiVeremi@users.noreply.github.com>
2025-02-18 22:54:33 +0100

    add

--- task1.py
+++ task1.py
@@ -8,60 +8,62 @@ logging.basicConfig(
 
 class Vehicle(ABC):
 
-    def __init__(self, make, model, spec):
+    def __init__(self, make: str, model: str, spec: str) -> None:
         self.make = make
         self.model = model
         self.spec = spec
 
     @abstractmethod
-    def start_engine(self):
+    def start_engine(self) -> None:
         pass
 
 
 class Car(Vehicle):
 
-    def start_engine(self):
+    def start_engine(self) -> None:
         logging.info(f"{self.make} {self.model} ({self.spec} Spec): Мотор заведено")
 
 
 class Motorcycle(Vehicle):
 
-    def start_engine(self):
+    def start_engine(self) -> None:
         logging.info(f"{self.make} {self.model} ({self.spec} Spec): Мотор заведено")
 
 
 class VehicleFactory(ABC):
 
     @abstractmethod
-    def create_car(self, make, model):
+    def create_car(self, make: str, model: str) -> Car:
         pass
 
     @abstractmethod
-    def create_motorcycle(self, make, model):
+    def create_motorcycle(self, make, model) -> Motorcycle:
         pass
 
 
 class USVehicleFactory(VehicleFactory):
-    def create_car(self, make, model):
+    def create_car(self, make: str, model: str) -> Car:
         return Car(make, model, "US")
 
-    def create_motorcycle(self, make, model):
+    def create_motorcycle(self, make: str, model: str) -> Motorcycle:
         return Motorcycle(make, model, "US")
 
 
 class EUVehicleFactory(VehicleFactory):
-    def create_car(self, make, model):
+
+    def create_car(self, make: str, model: str) -> Car:
         return Car(make, model, "EU")
 
-    def create_motorcycle(self, make, model):
+    def create_motorcycle(self, make: str, model: str) -> Motorcycle:
         return Motorcycle(make, model, "EU")
 
 
-us_factory = USVehicleFactory()
-eu_factory = EUVehicleFactory()
+if __name__ == "__main__":
+    us_factory = USVehicleFactory()
+    eu_factory = EUVehicleFactory()
 
-vehicle1 = us_factory.create_car("Ford", "Mustang")
-vehicle1.start_engine()
+    vehicle1 = us_factory.create_car("Ford", "Mustang")
+    vehicle1.start_engine()
 
-vehicle2 = eu_factory.create_motorcycle("Ducati", "ZR SUperSport")
-vehicle2.start_engine()
+    vehicle2 = eu_factory.create_motorcycle("Ducati", "ZR SUperSport")
+    vehicle2.start_engine()
